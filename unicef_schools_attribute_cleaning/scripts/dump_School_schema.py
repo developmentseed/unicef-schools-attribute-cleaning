6f57c375673257b1d3f9a8effc42b36c078779cd
Alex G Rice <agr@developmentseed.org>
2020-08-24 15:00:43 -0600

    wip

--- unicef_schools_attribute_cleaning/scripts/dump_School_schema.py
+++ unicef_schools_attribute_cleaning/scripts/dump_School_schema.py
@@ -4,12 +4,18 @@
 Dump the Pydantic model's fields into a json file.
 This is a hack/workaround for the fuzzy_alias_generator.py module having a circular import with the School.py.
 """
+
+import inspect
+import os
+
 from unicef_schools_attribute_cleaning.models.School import School
 
 
 def _main():
+    models_dir = os.path.dirname(inspect.getfile(School))
     schema = School.schema_json()
-    with open("../models/School_schema.json", "w") as fp:
+    target = f"{models_dir}/School_schema.json"
+    with open(target, "w") as fp:
         fp.write(schema)
 
 
